6d30c74d7c34509da25ec0e4367f40f8f1701889
Mohamed Hassan <mhass405@gmail.com>
2021-04-08 01:31:18 +0000

    pair programming excercise with nelly

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,11 +15,10 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
-
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -31,8 +30,17 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+  it("should return false if the letter array is empty", function() {
+    const result = wordSearch([], 'SEINFELD');
+    assert.isFalse(result);
+  });
+
+  it("should return false if the array is undefined", function() {
+    const result = wordSearch(undefined, 'SEINFELD');
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (!letters) {
+    return false;
+  }
+  if (!letters.length) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const check = (array) => {
+    for (let l of array) {
+      if (l.includes(word)) {
+        return true;
+      }
     }
-}
+    return false;
+  };
 
-module.exports = wordSearch
+  let col = letters.length;
+  let row = letters[0].length;
+  let verticalJoin = [];
+
+  if (col > row) {
+    let temp = 0;
+    temp = row;
+    row = col;
+    col = temp;
+  }
+
+  for (let i = 0; i < col; i++) {
+    let string = '';
+    for (let j = 0; j < row; j++) {
+      string += letters[j][i];
+    }
+    verticalJoin.push(string);
+    string = '';
+  }
+  if (check(horizontalJoin) === true || check(verticalJoin) === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = wordSearch;
